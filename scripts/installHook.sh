b272ce33b9802ea7fdd2056c357426466f2404c5
DaveBarnet <david.barnet@nhs.net>
2026-01-27 10:06:50 +0000

    Release of change to add communicationRequestVersion

--- scripts/installHook.sh
+++
@@ -1,5 +0,0 @@
-echo "Installing pre-commit hook to remove byte-order-markers when committing files"
-cd ..
-ln -s -f ../../scripts/pre-commit-remove-bom .git/hooks/pre-commit
-echo "Commit hook installed"
-
